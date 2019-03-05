2e445fcc1afb0ba94a340a808839bf5d975e86d9
ricardcutzh <ricardcutzh@gmail.com>
2019-03-05 00:23:08 -0600

    Agregando Datos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,12 @@
 							<div class="col-lg-5 col-md-6">
 								<div class="banner-content">
 									<h1>Nike New <br>Collection!</h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-										dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+									<p> Ricardo Antonio Cutz Hernandez <strong> 201503476 </strong> </p>
+									<p> Jhoseff Omar Caceres Aguilar <strong> 201513696 </strong> </p>
+									<p> Jorge Mario Recinos Dieguez <strong> 201314631 </strong> </p>
+									<p> Marvin José Calderón García <strong> 201213177 </strong> </p>
+									<p> Christopher Alexander López Orellana <strong> 201504100 </strong> </p>
+									<p> José Javier Barreda Mancilla <strong> 201213619 </strong> </p>
 									<div class="add-bag d-flex align-items-center">
 										<a class="add-btn" href=""><span class="lnr lnr-cross"></span></a>
 										<span class="add-text text-uppercase">Add to Bag</span>
